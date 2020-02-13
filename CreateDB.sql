eb470345784a6403fa312a0484911634b7870a76
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-02-13 11:56:00 +0100

    Update SQL files

--- CreateDB.sql
+++ CreateDB.sql
@@ -12,7 +12,7 @@ CREATE TABLE Employee(
   Hash VARBINARY(2048) NULL
 )
 
-INSERT INTO Employee VALUES 
+INSERT INTO Employee VALUES
   ('Admin', CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX)))
 ,  ('CEO', CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX)))
 ,  ('Consultant', CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX))+CAST(NEWID() AS VARBINARY(MAX)))
@@ -27,7 +27,7 @@ CREATE TABLE Customer(
 	AccountBalance MONEY NOT NULL DEFAULT 0
 )
 INSERT INTO Customer VALUES
-  ('Pavel', 'Secret!','He''s bad, do not borrow him money!', -100)
+  ('Pavel', 'Secret!','He''s bad, do not lend him money!', -100)
 , ('Alet', '123456', 'Verified', 10000)
 , ('Alex', 'cat', 'Verified', 10000)
 , ('Amélie', 'dog', 'Verified', 10000)
@@ -36,6 +36,7 @@ INSERT INTO Customer VALUES
 , ('Carine', 'dog', 'Verified', 10000)
 , ('Christelle', '123456', 'Verified', 10000)
 , ('Elena', 'cat', 'Verified', 10000)
+, ('Elodie', 'cat', 'Verified', 10000)
 , ('Elsa', 'dog', 'Verified', 10000)
 , ('Emma', '123456', 'Verified', 10000)
 , ('Emre', 'cat', 'Verified', 10000)
@@ -80,7 +81,7 @@ CREATE TABLE Article(
 	Text NVARCHAR(MAX) NOT NULL DEFAULT '',
 )
 
-INSERT INTO Article VALUES 
+INSERT INTO Article VALUES
   (0, 'Test', 'Test', 'This is test article')
 ,	(1, '', 'Security is priority', 'We pay close attantion to <b>security</b> and protecting your investments!<br /><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum, nunc nec varius hendrerit, felis sem molestie orci, in fringilla lacus est quis augue. Vestibulum condimentum bibendum quam sed finibus. Vestibulum hendrerit, nisi non vulputate posuere, leo nunc lobortis sem, in porta est turpis non dui. Duis diam felis, molestie in tempor et, aliquet ut magna. Proin pharetra est non leo vestibulum, a lobortis lacus placerat. Vivamus sem velit, bibendum eu lorem quis, finibus porta eros. Etiam ex nisl, pharetra quis laoreet at, condimentum nec nibh. Praesent mattis metus orci, eu dignissim sapien commodo nec. Pellentesque non dui bibendum, elementum erat vitae, dignissim libero. Nulla ut lacinia augue, eget mattis ante. Cras condimentum tellus sit amet efficitur aliquet. Sed tincidunt dolor urna, eget rhoncus sem pharetra eget. Phasellus feugiat, lectus et sodales porttitor, quam libero ultrices est, id elementum quam enim id urna. Etiam semper neque a risus faucibus varius. Interdum et malesuada fames ac ante ipsum primis in faucibus.')
 , (1, 'About', 'About us', 'We''re the <b>best</b> and most <b>secure</b> bank ever!<br /><br />Nunc aliquam euismod ante id porta. Donec eget iaculis metus. Vestibulum velit lorem, cursus iaculis lorem vitae, varius semper nulla. Mauris congue hendrerit ante sed lobortis. Proin eu varius sem, sed pellentesque nunc. Donec maximus diam vel mi condimentum euismod. Cras in sagittis lectus. Sed et condimentum mi. Nulla posuere ac purus eget pharetra. Aenean elementum vestibulum leo in tempor. Sed at orci efficitur, sodales urna eu, varius risus. Integer sagittis luctus mollis. Suspendisse rutrum enim at elit imperdiet accumsan.<br /><br />Cras posuere orci eu lorem bibendum faucibus. Aenean placerat libero et urna tempor, eu posuere augue lobortis. Duis vehicula dignissim libero, id consequat metus ultricies eu. Curabitur varius consectetur dui, ac commodo libero semper nec. Suspendisse felis felis, consequat sit amet orci a, consectetur vehicula dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi ornare libero auctor pharetra consequat. Aliquam sed turpis purus. Mauris convallis justo aliquam viverra eleifend. Donec vitae neque mauris.')
---
+++ Intro.sql
@@ -0,0 +1,45 @@
+
+SELECT * FROM Customer
+
+
+
+SELECT ID, Login, Reputation FROM Customer
+
+
+
+SELECT * FROM Customer WHERE Login='Pavel'
+
+
+
+SELECT Reputation FROM Customer WHERE Login='Pavel'
+
+
+
+SELECT ID FROM Customer WHERE Login='Pavel' AND Password='xxx'
+
+
+
+SELECT ID FROM Customer WHERE Login='Pavel' AND Password='Secret!'
+
+
+
+--This is comment
+
+
+
+SELECT Hack THIS site %%!
+
+
+
+SELECT 'Hack THIS site %%!'
+
+
+
+/*
+INSERT
+UPDATE
+DELETE
+
+Manage database (CREATE TABLE, DROP TABLE, ...)
+Manage server (SHUTDOWN, add login, ...)
+*/
